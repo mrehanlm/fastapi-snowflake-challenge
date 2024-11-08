30e76cb1e59b00534a49102603991316358482c2
M. Rehan <mrehanml603@gmail.com>
2024-11-08 17:04:18 +0500

    Initial commit

---
+++ fastapi_snowflake_challenge/services/db.py
@@ -0,0 +1,32 @@
+from contextlib import asynccontextmanager
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+
+from ..settings import settings
+
+# SQLAlchemy async engine requires non-standard driver DSN that don't work with other libraries.
+# We use the standard but transform it for the async engine.
+engine_mappings = {
+    "sqlite": "sqlite+aiosqlite",
+    "postgresql": "postgresql+asyncpg",
+}
+
+db_url = settings.database_url
+for find, replace in engine_mappings.items():
+    db_url = db_url.replace(find, replace)
+
+
+engine = create_async_engine(db_url, future=True, echo=settings.debug)
+
+
+@asynccontextmanager
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
+    async_session = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+    async with async_session() as session:
+        yield session
+
+
+async def get_session_depends() -> AsyncGenerator[AsyncSession, None]:
+    async with get_session() as session:
+        yield session
